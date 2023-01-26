e3e7d96d9c3df9bf52bf6a0a5220b46c9809c4de
AdamboJnr <AdamboAllan75@gmail.com>
2023-01-26 23:34:27 +0300

    Word counter app

---
+++ WordCounter/src/components/WordCounter.vue
@@ -0,0 +1,13 @@
+<script scoped>
+    export default{
+        data(){
+            return{
+                text: ""
+            }
+        }
+    }
+</script>
+<template>
+    <input type="text" v-model="text"><br>
+    <p>Words: {{ text.length }}</p>
+</template>
---
+++ WordCounter/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import WordCounter from '../components/WordCounter.vue'
+</script>
+
+<template>
+  <WordCounter />
+</template>
